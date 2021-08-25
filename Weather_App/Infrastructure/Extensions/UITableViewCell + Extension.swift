d76cc8c9bbb9e580983746ec22eeb820c3289e52
BakhvaJakeli <lordbaxva@gmail.com>
2021-08-25 16:34:43 +0400

    File gadanawileba

---
+++ Weather_App/Infrastructure/Extensions/UITableViewCell + Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UITableViewCell + Extension.swift
+//  BookAppTeamWork
+//
+//  Created by MacBook  on 08.07.21.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+    
+    static func nib() -> UINib {
+        return UINib(nibName: String(describing: self), bundle: Bundle.main)
+    }
+}
